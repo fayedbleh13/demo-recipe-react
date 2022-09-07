61551c59b6e4d23c0347d8467294b94739ff6104
Fayed Mauyag <faemauyag13@gmail.com>
2022-09-08 01:03:30 +0800

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCV8Bwzn_-fkeLQXgVAopCrVQwWS2k5VyI",
+  authDomain: "fir-recipe-webapp.firebaseapp.com",
+  projectId: "fir-recipe-webapp",
+  storageBucket: "fir-recipe-webapp.appspot.com",
+  messagingSenderId: "179963218245",
+  appId: "1:179963218245:web:6b96f2b272aadaa89c962c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
