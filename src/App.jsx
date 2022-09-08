511f71cd0d80dbf75f4650ad2806bcf8f7d261ce
Fayed Mauyag <faemauyag13@gmail.com>
2022-09-08 14:08:06 +0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './Pages/Auth/Login'
+import Register from './Pages/Auth/Register'
+import Reset from './Pages/Auth/ForgotPassword'
+import Cover from './Pages/Cover'
+import Dashboard from './Pages/Dashboard'
+import './Assets/App.css'
 
 function App() {
-  
-  return (
-    <div className="App">
-      
-    </div>
-  )
+    return (
+        <div className='app'>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path='/' element={<Cover />}/>
+                    <Route exact path='/login' element={<Login />}/>
+                    <Route exact path='/register' element={<Register />}/>
+                    <Route exact path='/reset' element={<Reset />}/>
+                    <Route exact path='/dashboard' element={<Dashboard />}/>
+                    <Route exact path='*'/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
