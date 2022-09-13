bf5c01dd0028c4c9372a0f944b2f73088eeb3dbb
Fayed Mauyag <faemauyag13@gmail.com>
2022-09-13 15:29:53 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,108 +1,54 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import toast from 'react-hot-toast'
 
 import {
-    GoogleAuthProvider,
     getAuth,
-    signInWithPopup,
-    signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
     sendPasswordResetEmail,
-    signOut,
+    signOut
 } from "firebase/auth";
-
- import {
-    getFirestore,
-    query,
-    getDocs,
-    collection,
-    where,
-    addDoc,
-} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCV8Bwzn_-fkeLQXgVAopCrVQwWS2k5VyI",
-    authDomain: "fir-recipe-webapp.firebaseapp.com",
-    projectId: "fir-recipe-webapp",
-    storageBucket: "fir-recipe-webapp.appspot.com",
-    messagingSenderId: "179963218245",
-    appId: "1:179963218245:web:6b96f2b272aadaa89c962c"
-};
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const auth = getAuth();
 
-// Google auth
-const googleProvider = new GoogleAuthProvider();
-const signInWithGoogle = async () => {
-  try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    const docs = await getDocs(q);
-    if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
-        uid: user.uid,
-        name: user.displayName,
-        authProvider: "google",
-        email: user.email,
-      });
-    }
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
+//register user with firebase authentication
+export const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
 };
 
-// Email/PW auth
-const logInWithEmailAndPassword = async (email, password) => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      console.error(err);
-      alert(err.message);
-    }
+//login user with firebase authentication
+export const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
 };
 
-const registerWithEmailAndPassword = async (name, email, password) => {
-    try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
-      const user = res.user;
-      await addDoc(collection(db, "users"), {
-        uid: user.uid,
-        name,
-        authProvider: "local",
-        email,
-      });
-    } catch (err) {
-      console.error(err);
-      alert(err.message);
-    }
-};
+//password reset by email with firebase authentication
+export const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+}
 
-const sendPasswordReset = async (email) => {
-    try {
-      await sendPasswordResetEmail(auth, email);
-      alert("Password reset link sent!");
-    } catch (err) {
-      console.error(err);
-      alert(err.message);
-    }
-};
-
-const logout = () => {
-    signOut(auth);
-};
-
-export {
-    auth,
-    db,
-    signInWithGoogle,
-    logInWithEmailAndPassword,
-    registerWithEmailAndPassword,
-    sendPasswordReset,
-    logout,
+//logout user with firebase authentication
+export const logoutUser = () => {
+    signOut(auth).then(() => {
+        toast.success("Successfully signed out")
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        toast.error(`${errorCode}`)
+        console.log(errorCode);
+    })
 };
